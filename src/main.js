dc6179f65ae5648defabe4d1ed8444c4976a74e5
yangxin <1393003255@qq.com>
2024-09-16 15:21:41 +0800

    feat: 项目架构之搭建登录架构解决方案与实现-美化登录页面样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import installElementPlus from './plugins/element'
+// 初始化样式表
+import '@/styles/index.scss'
 
 const app = createApp(App)
 app.use(router)
