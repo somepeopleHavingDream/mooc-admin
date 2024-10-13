da1310128b041ede36a495b21222b55c5fefd5df
yangxin <1393003255@qq.com>
2024-10-13 20:04:14 +0800

    feat: 项目架构之搭建登录架构解决方案与实现-本地缓存处理方案

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,19 @@
 import { login } from '@/api/sys'
 import md5 from 'md5'
+import { setItem, getItem } from '@/utils/storage'
+import { TOKEN } from '@/constant'
 
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     /**
      * 登录请求动作
@@ -17,6 +26,7 @@ export default {
           password: md5(password)
         })
           .then((data) => {
+            this.commit('user/setToken', data.data.data.token)
             resolve()
           })
           .catch((err) => {
