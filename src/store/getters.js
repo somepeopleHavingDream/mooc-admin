7dc06f863ef296a42e4e2eaa35f3fbab53bda46f
yangxin <1393003255@qq.com>
2024-10-17 22:58:30 +0800

    feat: 项目架构之搭建Layout架构 解决方案与实现-渲染用户头像菜单

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,12 +1,13 @@
 // 快捷访问
 const getters = {
-  token: state => state.user.token,
+  token: (state) => state.user.token,
   /**
    * @returns true 表示用户信息已存在
    */
-  hasUserInfo: state => {
+  hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  userInfo: state => state.user.userInfo
 }
 
 export default getters
