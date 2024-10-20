04e283e0b74225043ee3b2de6d15c995503df982
yangxin <1393003255@qq.com>
2024-10-20 20:36:51 +0800

    feat: 项目架构之搭建Layout架构 解决方案与实现-业务落地：创建结构路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,65 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import layout from '@/layout/index'
+
+/**
+ * 私有路由表
+ */
+const privateRoutes = [
+  {
+    path: '/user',
+    component: layout,
+    redirect: '/user/manage',
+    meta: {
+      title: 'user',
+      icon: 'personal'
+    },
+    children: [
+      {
+        path: '/user/manage',
+        name: 'userManage',
+        component: () => import('@/views/user-manage/index'),
+        meta: {
+          title: 'userManage',
+          icon: 'personal-manage'
+        }
+      },
+      {
+        path: '/user/role',
+        name: 'userRole',
+        component: () => import('@/views/role-list/index'),
+        meta: {
+          title: 'roleList',
+          icon: 'role'
+        }
+      },
+      {
+        path: '/user/permission',
+        name: 'userPermission',
+        component: () => import('@/views/permission-list/index'),
+        meta: {
+          title: 'permissionList',
+          icon: 'permission'
+        }
+      },
+      {
+        path: '/user/info/:id',
+        name: 'userInfo',
+        component: () => import('@/views/user-info/index'),
+        meta: {
+          title: 'userInfo'
+        }
+      },
+      {
+        path: '/user/import',
+        name: 'import',
+        component: () => import('@/views/import/index'),
+        meta: {
+          title: 'excelImport'
+        }
+      }
+    ]
+  }
+]
 
 /**
  * 公开路由表
@@ -10,13 +71,38 @@ const publicRoutes = [
   },
   {
     path: '/',
-    component: () => import('@/layout/index')
+    redirect: '/profile',
+    component: layout,
+    children: [
+      // 个人中心
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/profile/index'),
+        meta: {
+          title: 'profile',
+          icon: 'el-icon-user'
+        }
+      },
+      // 404
+      {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/error-page/404')
+      },
+      // 401
+      {
+        path: '/401',
+        name: '401',
+        component: () => import('@/views/error-page/401')
+      }
+    ]
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: publicRoutes
+  routes: [...publicRoutes, ...privateRoutes]
 })
 
 export default router
