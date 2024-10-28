efd9f431d025b0e03aa552850001b2feaebb54d0
yangxin <1393003255@qq.com>
2024-10-28 21:45:36 +0800

    feat: 项目架构之搭建Layout架构 解决方案与实现-业务落地：修复最后残余问题

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,12 +1,23 @@
 <template>
-  <el-menu :uniqueOpen="true" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-    <sidebar-item v-for="item in routes" :key="item.path" :route="item"></sidebar-item>
+  <el-menu
+    :default-active="activeMenu"
+    :uniqueOpen="true"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    router
+  >
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
   </el-menu>
 </template>
 
 <script setup>
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { filterRoutes, generateMenus } from '@/utils/route'
 import SidebarItem from './SidebarItem.vue'
 
@@ -15,8 +26,13 @@ const routes = computed(() => {
   const fRoutes = filterRoutes(router.getRoutes())
   return generateMenus(fRoutes)
 })
-console.log('routes.value', routes.value)
+
+// 默认激活
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
