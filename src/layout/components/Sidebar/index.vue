26f3b0b01b795b31f2d9c63ea85b8036df064a4d
yangxin <1393003255@qq.com>
2024-11-03 22:06:26 +0800

    feat: 项目架构之搭建Layout架构 解决方案与实现-全新 vue 能力：组件状态驱动的动态 css 值

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -2,6 +2,7 @@
   <div class="">
     <div class="logo-container">
       <el-avatar
+        :size="logoHeight"
         shape="square"
         src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
       ></el-avatar>
@@ -18,11 +19,13 @@
 <script setup>
 import SidebarMenu from './SidebarMenu.vue'
 import {} from 'vue'
+
+const logoHeight = 44
 </script>
 
 <style lang="scss" scoped>
 .logo-container {
-  height: 44px;
+  height: v-bind(logoHeight) + 'px';
   padding: 10px 0 22px 0;
   display: flex;
   align-items: center;
