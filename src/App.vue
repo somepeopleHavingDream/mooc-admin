a9ca14d3a1d0646582efb2dc1d0fa94d7495f5f1
yangxin <1393003255@qq.com>
2024-11-10 23:09:34 +0800

    feat: 后台项目前端综合解决方案之通用功能开发-方案落地：element-plus 新主题的立即生效

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script setup>
+import { useStore } from 'vuex'
+import { generateNewStyle, writeNewStyle } from './utils/theme'
+
+const store = useStore()
+generateNewStyle(store.getters.mainColor).then((newStyle) => {
+  writeNewStyle(newStyle)
+})
 </script>
 
-<style>
-</style>
+<style></style>
