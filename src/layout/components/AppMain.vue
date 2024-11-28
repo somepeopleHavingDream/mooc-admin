52574e77be2e41a63242dd6000690370c9efaf08
yangxin <1393003255@qq.com>
2024-11-28 21:38:54 +0800

    feat: 后台项目前端综合解决方案之通用功能开发-方案落地：tagsView 国际化处理

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -9,7 +9,7 @@ import { watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { isTags } from '@/utils/tags'
 import { useStore } from 'vuex'
-import { generateTitle } from '@/utils/i18n'
+import { generateTitle, watchSwitchLang } from '@/utils/i18n'
 
 /**
  * 生成 title
@@ -52,6 +52,18 @@ watch(
     immediate: true
   }
 )
+
+watchSwitchLang(() => {
+  store.getters.tagsViewList.forEach((route, index) => {
+    store.commit('app/changeTagsView', {
+      index,
+      tag: {
+        ...route,
+        title: getTitle(route)
+      }
+    })
+  })
+})
 </script>
 
 <style lang="scss" scoped>
