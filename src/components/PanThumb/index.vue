d91de5f1548b2a6f50ab4e39095b48e0c41e56f1
yangxin <1393003255@qq.com>
2025-03-09 17:13:17 +0800

    feat: vue 3 + element plus 综合实现解决方案与个人中心页面实现-创建 PanThumb 头像组件

---
+++ src/components/PanThumb/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="pan-item" :style="{ zIndex: zIndex, height: height, width: width }">
+    <!-- 内层 -->
+    <div class="pan-info">
+      <div class="pan-info-roles-container">
+        <slot />
+      </div>
+    </div>
+    <!-- 外层 -->
+    <div :style="{ backgroundImage: `url(${image})`}" class="pan-thumb"></div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  image: {
+    type: String
+  },
+  zIndex: {
+    type: Number,
+    default: 1
+  },
+  width: {
+    type: String,
+    default: '150px'
+  },
+  height: {
+    type: String,
+    default: '150px'
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.pan-item {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+
+  .pan-info {
+    position: absolute;
+    width: inherit;
+    border: inherit;
+    border-radius: 50%;
+    overflow: hidden;
+    box-shadow: inset 0 0 0 5px rgba(0, 0, 0, 0.05);
+
+    .pan-info-roles-container {
+      padding: 20px;
+      text-align: center;
+    }
+  }
+
+  .pan-thumb {
+    width: 100%;
+    height: 100%;
+    background-position: center center;
+    background-size: cover;
+    border-radius: 50%;
+    overflow: hidden;
+    position: absolute;
+    transform-origin: 95% 40%;
+    transition: all 0.3s ease-in-out;
+  }
+}
+
+.pan-item:hover .pan-thumb {
+  transform: rotate(-110deg);
+}
+
+.pan-item:hover .pan-info p a {
+  opacity: 1;
+  transform: translateX(0px) rotate(0deg);
+}
+</style>
