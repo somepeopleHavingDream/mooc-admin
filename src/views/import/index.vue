439e8a71f38bce7cdfafb0803e1f42e3ecf7643b
yangxin <1393003255@qq.com>
2025-03-26 21:42:53 +0800

    feat: excel zip 与前端结合解决方案之用户管理页面实现 - 业务落地：提供两种文件导入形式

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="">导入</div>
+  <upload-excel></upload-excel>
 </template>
 
 <script setup>
 import {} from 'vue'
+import UploadExcel from '@/components/UploadExcel'
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -2,7 +2,7 @@
   <div class="user-manage-container">
     <el-card class="header">
       <div>
-        <el-button type="primary">{{ $t('msg.excel.importExcel') }}</el-button>
+        <el-button type="primary" @click="onImportExcelClick">{{ $t('msg.excel.importExcel') }}</el-button>
         <el-button type="success">{{ $t('msg.excel.exportExcel') }}</el-button>
       </div>
     </el-card>
@@ -87,6 +87,7 @@
 import { ref } from 'vue'
 import { getUserManageList } from '@/api/user-manage'
 import { watchSwitchLang } from '@/utils/i18n'
+import { useRouter } from 'vue-router'
 
 // 数据相关
 const tableData = ref([])
@@ -109,6 +110,13 @@ watchSwitchLang(getListData)
 
 const handleSizeChange = () => {}
 const handleCurrentChange = () => {}
+
+// excel 导入按钮点击事件
+const router = useRouter()
+const onImportExcelClick = () => {
+  router.push('/user/import')
+}
+
 </script>
 
 <style lang="scss" scoped>
